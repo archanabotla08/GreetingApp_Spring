557776d2a9ab3c4a2983fe3d6f00cf7b1e2c0ce3
archanabotla08 <archana.botla@gmail.com>
2020-12-22 12:36:27 +0530

    [Archana] Added: UC2 to get method with query parameter

--- src/main/java/com/example/demo/greetingapp/controller/HelloRestController.java
+++ src/main/java/com/example/demo/greetingapp/controller/HelloRestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.greetingapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,8 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
 	
 	@RequestMapping(value= {"","/","/home"})
-	public String sayHello() {
+	public String HelloWorld() {
 		return "HelloWorld from rest";
 	}
+	//http://localhost:8080/hellorestcontrollerss/query/?fname=archana&lname=botla
+		@RequestMapping(value = { "/queryName" }, method = RequestMethod.GET)
+		public String sayHelloName(@RequestParam("fname") String fname, @RequestParam("lname") String lname) {
+			return "Hello " + fname + " " + lname;
+		}
 	
+	//http://localhost:8080/hellorestcontrollerss/query/?fname=archana&lname=botla
+	@RequestMapping(value = { "/query" }, method = RequestMethod.GET)
+	public String sayHello(@RequestParam("fname") String fname, @RequestParam("lname") String lname) {
+		return "Hello " + fname + " " + lname;
+	}
 }
